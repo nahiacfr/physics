c377e94789428507dff00f952425cc8bce6cd94a
nahiacfr <nahia.castaneda@opendeusto.es>
2023-03-10 08:45:30 +0100

    mover

---
+++ Assets/Mover.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    private Rigidbody riggidBody;
+    private bool canJump;
+    [SerializeField]
+    public int jumpForce = 200;
+    [SerializeField]
+    float moveSpeed = 10f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        riggidBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        Jump();
+    }
+
+    private void Jump()
+    {
+        if (canJump && Input.GetKeyDown(KeyCode.Space))
+        {
+            riggidBody.AddForce(new Vector3(0, jumpForce, 0));
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        canJump = true;
+    }
+    void OnCollisionExit(Collision collision)
+    {
+        canJump = false;
+    }
+
+    private void Move()
+    {
+
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(horizontalInput, 0f, verticalInput);
+        transform.position += movement * moveSpeed * Time.deltaTime;
+    }
+
+}
