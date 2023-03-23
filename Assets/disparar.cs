6b565df6c5f0235868920eb7c4c75d9552026470
nahiacfr <nahia.castaneda@opendeusto.es>
2023-03-23 09:07:26 +0100

    Raycast

--- Assets/disparar.cs
+++ Assets/disparar.cs
@@ -13,7 +13,27 @@ public class disparar : MonoBehaviour
     {
 
     }
+    void FixedUpdate()
+    {
+        // Bit shift the index of the layer (8) to get a bit mask
+        int layerMask = 1 << 6;
 
+        // This would cast rays only against colliders in layer 8.
+        // But instead we want to collide against everything except layer 8. The ~ operator does this, it inverts a bitmask.
+
+        RaycastHit hit;
+        // Does the ray intersect any objects excluding the player layer
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity, layerMask))
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.red);
+            Debug.Log("Did Hit" + hit.collider.gameObject.name);
+        }
+        else
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 1000, Color.white);
+            Debug.Log("Did not Hit");
+        }
+    }
     // Update is called once per frame
     void Update()
     {
@@ -23,7 +43,7 @@ public class disparar : MonoBehaviour
 
             rbullet.GetComponent<Rigidbody>().AddForce(new Vector3(force, 0, 0));
 
-            
+
         }
         if (collisiontrue)
         {
@@ -42,3 +62,5 @@ public class disparar : MonoBehaviour
 
     }
 }
+
+    
